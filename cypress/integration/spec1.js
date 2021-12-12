cc33c98367ad9676325b1edda48eb336009eeeab
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2021-12-11 19:49:51 -0500

    use cy-spok

--- cypress/integration/spec1.js
+++ cypress/integration/spec1.js
@@ -1,22 +1,20 @@
 // enables intelligent code completion for Cypress commands
 // https://on.cypress.io/intelligent-code-completion
 /// <reference types="cypress" />
+import spok from 'cy-spok'
 
 describe('Express server 1', () => {
   it('responds', () => {
     const url = Cypress.env('url')
     const port = Cypress.env('port')
-    cy.request(url).its('body').should('deep.include', {
-      message: 'Hello World!',
-      port,
-    })
-  })
-
-  it('responds with random id', () => {
-    const url = Cypress.env('url')
     cy.request(url)
-      .its('body.responseId')
-      .should('be.a', 'number')
-      .and('be.within', 1e5, 1e6)
+      .its('body')
+      .should(
+        spok({
+          message: 'Hello World!',
+          port,
+          responseId: spok.range(1e5, 1e6),
+        }),
+      )
   })
 })
